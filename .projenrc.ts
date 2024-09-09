04c51d56b712082a03d39a7281538caf3ec30274
Matteo Restelli <teoreste@gmail.com>
2024-09-09 09:19:03 +0200

    feat: CDK lib bump and fixing Github Actions upload artifacts (#237)
    - Bumping CDK version
    - Fixing #236

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,7 +4,7 @@ const project = new CdklabsConstructLibrary({
   authorAddress: 'aws-avp-cdk-dev@amazon.com',
   description: 'L2 AWS CDK Constructs for Amazon Verified Permissions',
   keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws', 'verified-permissions', 'authorization', 'verifiedpermissions'],
-  cdkVersion: '2.148.0',
+  cdkVersion: '2.156.0',
   defaultReleaseBranch: 'main',
   devDeps: ['cdklabs-projen-project-types'],
   bundledDeps: ['@cedar-policy/cedar-wasm@3.2.3'],
